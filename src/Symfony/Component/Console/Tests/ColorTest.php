d7f1ee043a1d677652c159f06b0b4c1362c91e57
Julien Boudry <julien.boudry@gmail.com>
2022-07-15 22:20:35 +0200

    [Console] Split ColorTest::testDegradedTrueColor() in two

--- src/Symfony/Component/Console/Tests/ColorTest.php
+++ src/Symfony/Component/Console/Tests/ColorTest.php
@@ -13,7 +13,6 @@ namespace Symfony\Component\Console\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Color;
-use Symfony\Component\Console\Terminal;
 
 class ColorTest extends TestCase
 {
@@ -48,32 +47,37 @@ class ColorTest extends TestCase
         }
     }
 
-    public function testDegradedTrueColors()
+    public function testDegradedTrueColorsToAnsi4()
     {
         $colorterm = getenv('COLORTERM');
         $term = getenv('TERM');
 
-        try {
-            // To Ansi4
-            putenv('COLORTERM=');
-            putenv('TERM=');
+        putenv('COLORTERM=');
+        putenv('TERM=');
 
+        try {
             $color = new Color('#f00', '#ff0');
             $this->assertSame("\033[31;43m \033[39;49m", $color->apply(' '));
 
             $color = new Color('#c0392b', '#f1c40f');
             $this->assertSame("\033[31;43m \033[39;49m", $color->apply(' '));
+        } finally {
+            (false !== $colorterm) ? putenv('COLORTERM='.$colorterm) : putenv('COLORTERM');
+            (false !== $term) ? putenv('TERM='.$term) : putenv('TERM');
+        }
+    }
 
-            // To Ansi 8, parsed from $TERM
-            putenv('COLORTERM=');
-            putenv('TERM=SymfonyTermTest-256color'); // Real example: xterm-256
+    public function testDegradedTrueColorsToAnsi8()
+    {
+        $colorterm = getenv('COLORTERM');
+        $term = getenv('TERM');
 
-            $color = new Color('#f57255', '#8993c0'); // 210
+        putenv('COLORTERM=');
+        putenv('TERM=symfonyTest-256color');
+
+        try {
+            $color = new Color('#f57255', '#8993c0');
             $this->assertSame("\033[38;5;210;48;5;146m \033[39;49m", $color->apply(' '));
-
-            // To Ansi 8, parsed from $COLORTERM
-            putenv('COLORTERM=256color');
-            putenv('TERM=SymfonyTermTest'); // Real example: xterm-256
 
             $color = new Color('#000000', '#ffffff');
             $this->assertSame("\033[38;5;16;48;5;231m \033[39;49m", $color->apply(' '));
